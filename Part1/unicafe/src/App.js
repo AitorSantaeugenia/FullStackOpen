f5a643100653288f3a6c72ae87d6ea174bb71ba5
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-05 03:01:51 +0200

    feat: part1 unicafe 1.8

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -7,24 +7,14 @@ const Button = (props) => (
   </button>
 );
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  let sum = 0;
-
-  const setToGood = (good) => setGood(good);
-  const setToNeutral = (neutral) => setNeutral(neutral);
-  const setToBad = (bad) => setBad(bad);
-  sum = good + neutral + bad;
+// a proper place to define a component
+const Statistics = ({ good, neutral, bad }) => {
+  let sum = good + neutral + bad;
+  let average = (good - bad) / sum;
+  let positive = (good / sum) * 100;
 
   return (
     <div>
-      <h2>give feedback</h2>
-      <Button handleClick={() => setToGood(good + 1)} text="good" />
-      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
       <h2>statistics</h2>
       <span>good {good}</span>
       <br></br>
@@ -34,9 +24,31 @@ const App = () => {
       <br></br>
       <span>all {sum ? sum : 0}</span>
       <br></br>
-      <span>average {good ? (good - bad) / sum : 0}</span>
+      <span className={average > 0 ? "spanGreen" : "spanRed"}>
+        average {average ? average : 0}
+      </span>
       <br></br>
-      <span>positive {good ? (good / sum) * 100 + " %" : 0}</span>
+      <span>positive {positive ? positive + " %" : 0}</span>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const setToGood = (good) => setGood(good);
+  const setToNeutral = (neutral) => setNeutral(neutral);
+  const setToBad = (bad) => setBad(bad);
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
