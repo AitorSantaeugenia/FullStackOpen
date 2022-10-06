92a0b8d572ed09c4a7700cf6d7bc2dd32ad30f11
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-06 19:29:48 +0200

    part2 phonebook 2.8 step3

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,14 +2,20 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addNewPerson = (e) => {
     e.preventDefault();
 
+    console.log(newNumber);
+
     const newObject = {
       name: newName,
+      number: newNumber,
     };
 
     if (persons.find((person) => person.name === newObject.name)) {
@@ -19,6 +25,7 @@ const App = () => {
     } else {
       setPersons(persons.concat(newObject));
       setNewName("");
+      setNewNumber("");
     }
   };
 
@@ -28,23 +35,45 @@ const App = () => {
     setNewName(e.target.value);
   };
 
+  const handlePhoneNumber = (e) => {
+    e.preventDefault();
+    console.log("e", e.target.value);
+
+    setNewNumber(e.target.value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
         <div>
-          name: <input value={newName} onChange={handleNewName} />
+          <span className="spanPaddingRight">name:</span>{" "}
+          <input
+            value={newName}
+            onChange={handleNewName}
+            className="inputMarginLeft"
+          />
+        </div>
+        <div>
+          <span className="spanPaddingRight">number:</span>{" "}
+          <input value={newNumber} onChange={handlePhoneNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-      </ul>
+
+      {persons.map((person) => (
+        <tr>
+          <td key={person.name} className="tdPadding">
+            {person.name}
+          </td>
+          <td key={person.number} className="tdPaddingLeft">
+            {person.number}
+          </td>
+        </tr>
+      ))}
     </div>
   );
 };
