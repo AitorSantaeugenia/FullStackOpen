12ef585f5609641026033d54f51303fa81475f29
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-06 17:41:07 +0200

    feat: courseinfo 2.5 separated module

--- Part2/courseinfo/src/index.js
+++ Part2/courseinfo/src/index.js
@@ -1,58 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const Header = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-const Part = ({ name, exercises, total }) => {
-  return (
-    <div>
-      <p>
-        {name} {exercises}
-      </p>
-    </div>
-  );
-};
-const Content = (props) => {
-  let arr = [];
-  let total = 0;
-  return (
-    <div>
-      {props.parts.map((course) => {
-        arr.push(course.exercises);
-        total = arr.reduce((s, p) => s + p, 0);
-        return (
-          <Part
-            name={course.name}
-            exercises={course.exercises}
-            total={total}
-            key={course.id}
-          />
-        );
-      })}
-      <Total total={total} />
-    </div>
-  );
-};
-
-const Total = ({ total }) => {
-  return <p className="boldText">number of {total} exercises</p>;
-};
-
-const Course = ({ course }) => (
-  <div>
-    {course.map((course) => {
-      return (
-        <>
-          <Header course={course.name} key={course.id} />
-          <Content parts={course.parts} />
-        </>
-      );
-    })}
-  </div>
-);
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
