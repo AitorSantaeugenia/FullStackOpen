38620aed6ee2b40237f3d8e7964e615ba0e34f13
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-06 17:09:22 +0200

    feat: part2 2.2 & 2.3 (with reducer), step7 and step8

--- Part2/courseinfo/src/index.js
+++ Part2/courseinfo/src/index.js
@@ -1,47 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Part = (props) => {
+const Part = ({ name, exercises, total }) => {
   return (
     <div>
       <p>
-        {props.name} {props.exercises}
+        {name} {exercises}
       </p>
     </div>
   );
 };
 const Content = (props) => {
+  let arr = [];
+  let total = 0;
   return (
     <div>
-      {props.parts.map((course) => (
-        <Part name={course.name} exercises={course.exercises} key={course.id} />
-      ))}
+      {props.parts.map((course) => {
+        arr.push(course.exercises);
+        total = arr.reduce((s, p) => s + p, 0);
+        return (
+          <Part
+            name={course.name}
+            exercises={course.exercises}
+            total={total}
+            key={course.id}
+          />
+        );
+      })}
+      <Total total={total} />
     </div>
   );
 };
 
 //Not using total in this exercises, at least not yet
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercises:{" "}
-        {parseInt(
-          props.parts[0].exercises +
-            props.parts[1].exercises +
-            props.parts[2].exercises
-        )}
-      </p>
-    </div>
-  );
+const Total = ({ total }) => {
+  return <p className="boldText">number of {total} exercises</p>;
 };
 
 const Course = ({ course }) => (
   <div>
+    {/* {console.log(course)} */}
     <Header course={course.name} />
     <Content parts={course.parts} />
   </div>
