53ab9d637d156b4d9d31bff01d8cd23f4fbc6a76
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-07 19:35:40 +0200

    feat: part2 phonebook 2.17 step 9 delete

--- Part2/phonebook/src/components/PersonData.jsx
+++ Part2/phonebook/src/components/PersonData.jsx
@@ -1,10 +1,18 @@
-const PersonData = (person) => (
+const PersonData = ({person, handleDelete}) => (
+  
     <tr>
       <td key={person.name} className="tdPadding">
         {person.name}
       </td>
+      {/* we added the id to check it easier into the db.json */}
+      <td key={person.id} className="tdPadding">
+        id: {person.id}
+      </td>
       <td key={person.number} className="tdPaddingLeft">
         {person.number}
+      </td>
+      <td className="tdPaddingLeft">
+        <button className="btnPhonebook" onClick={() => handleDelete(person.id)}>Remove</button>
       </td>
     </tr>
   );
