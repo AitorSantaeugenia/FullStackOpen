557a2c4760dab5abe62b5e90dac04b44055f07a0
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-07 20:26:34 +0200

    feat: part2 2.18 step10 update service

--- Part2/phonebook/src/services/services.js
+++ Part2/phonebook/src/services/services.js
@@ -9,11 +9,9 @@ const create = (newObject) => {
   return axios.post(baseUrl, newObject);
 };
 
-//dunno if we will need to updated in this part, so we are saving this here
-//from: d) Altering data in server - Extracting Communication with the Backend into a Separate Module
-// const update = (id, newObject) => {
-//   return axios.put(`${baseUrl}/${id}`, newObject);
-// };
+const update = (id, newObject) => {
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
 const remove = async (id) => {
   const response = await axios.delete(`${baseUrl}/${id}`);
@@ -24,5 +22,5 @@ export default {
   getAll: getAll,
   create: create,
   remove: remove,
-  //   update: update,
+  update: update,
 };
