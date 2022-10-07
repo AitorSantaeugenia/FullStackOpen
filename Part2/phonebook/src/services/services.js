5463145baa1bd18137374da19f0be49b62f9f4ec
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-07 18:14:45 +0200

    feat: part2 2.16 step8 added services

---
+++ Part2/phonebook/src/services/services.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3000/persons";
+
+const getAll = () => {
+  return axios.get(baseUrl);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject);
+};
+
+//dunno if we will need to updated in this part, so we are saving this here
+//from: d) Altering data in server - Extracting Communication with the Backend into a Separate Module
+// const update = (id, newObject) => {
+//   return axios.put(`${baseUrl}/${id}`, newObject);
+// };
+
+export default {
+  getAll: getAll,
+  create: create,
+  //   update: update,
+};
