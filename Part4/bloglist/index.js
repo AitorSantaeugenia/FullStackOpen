4cd7e0a274be1202ac7b67e6336d90ae446d2450
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-17 20:42:02 +0200

    refractor: 4.2 bloglist step2 refractor done

--- Part4/bloglist/index.js
+++ Part4/bloglist/index.js
@@ -1,50 +1,11 @@
-// const http = require("http");
+const http = require("http");
+//utils
+const logger = require("./utils/logger");
 const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const app = require("./app");
+const server = http.createServer(app);
 
-//we added this cost POST with rest client is not working
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    console.log(result);
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
