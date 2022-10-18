d9a8f6bce796f59fc5b0f471a8fcf66d4bfd548c
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-18 22:33:57 +0200

    feat:part4 4.10 step3 bloglist

--- Part4/bloglist/controllers/blogs.js
+++ Part4/bloglist/controllers/blogs.js
@@ -1,13 +1,25 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const logger = require("../utils/logger");
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response) => {
-  const savedBlog = Blog.save();
+blogsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  const savedBlog = await blog.save();
+  logger.info(`added ${blog.title} to the blog list`);
+
   response.status(201).json(savedBlog.toJSON());
 });
 
--- Part4/bloglist/tests/blogApi.test.js
+++ Part4/bloglist/tests/blogApi.test.js
@@ -38,6 +38,27 @@ describe("(4.9*, step2) - id definition:", () => {
   });
 });
 
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Aitor's Github",
+    author: "Aitor J. Santaeugenia Marí",
+    url: "https://github.com/AitorSantaeugenia",
+    likes: 10,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const contents = blogsAtEnd.map((n) => n.title);
+  expect(contents).toContain("Canonical string reduction");
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
