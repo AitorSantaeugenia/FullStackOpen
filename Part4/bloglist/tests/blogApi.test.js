49a283277af150be115fbaaf99a45f225d2174c6
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-10-19 02:01:51 +0200

    feat: 4.11* bloglist step4

--- Part4/bloglist/tests/blogApi.test.js
+++ Part4/bloglist/tests/blogApi.test.js
@@ -30,7 +30,7 @@ describe("(4.8, step1) - API tests:", () => {
   });
 });
 
-describe("(4.9*, step2) - id definition:", () => {
+describe("(4.9*, step2) - id definition - GET /api/blogs:", () => {
   test("verify if the unique identifier property `id` exists", async () => {
     const response = await api.get("/api/blogs");
 
@@ -38,25 +38,50 @@ describe("(4.9*, step2) - id definition:", () => {
   });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "Aitor's Github",
-    author: "Aitor J. Santaeugenia Marí",
-    url: "https://github.com/AitorSantaeugenia",
-    likes: 10,
-  };
+describe("(4.10, step3) - POST /api/blogs:", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Aitor's Github",
+      author: "Aitor J. Santaeugenia Marí",
+      url: "https://github.com/AitorSantaeugenia",
+      likes: 10,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsInDB = await helper.blogsInDb();
+    expect(blogsInDB).toHaveLength(helper.initialBlogs.length + 1);
 
-  const contents = blogsAtEnd.map((n) => n.title);
-  expect(contents).toContain("Canonical string reduction");
+    const contents = blogsInDB.map((n) => n.title);
+    expect(contents).toContain("Canonical string reduction");
+  });
+
+  test("if property likes is missing ", async () => {
+    const newBlog = {
+      title: "Aitor's Github 1",
+      author: "Aitor J. Santaeugenia Marí",
+      url: "https://github.com/AitorSantaeugenia",
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsInDB = await helper.blogsInDb();
+    const contents = await blogsInDB.find(
+      (blog) => blog.title === "Aitor's Github 1"
+    );
+
+    const likes = contents.likes ? contents.likes : 0;
+
+    expect(likes).toBe(0);
+  });
 });
 
 afterAll(() => {
